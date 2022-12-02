a8fe581e625926ce5d5398bb476ba9ef06f273e1
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-12-02 17:06:29 +0100

    added logout function to login/out element in menu

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,1 +1,13 @@
-//
+function logout() {
+
+    const loggoutBtn = document.querySelector("#log-in-out");
+
+    loggoutBtn.addEventListener("click", function(event) {
+        
+        event.preventDefault();
+        console.log(event);
+
+    });
+};
+
+logout();
