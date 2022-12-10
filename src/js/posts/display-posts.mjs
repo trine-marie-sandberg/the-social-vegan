2ad0c7fd6accd3bbfbffef179f96d5214b8235df
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-12-11 00:27:20 +0100

    displays posts and set placeholder images

--- src/js/posts/display-posts.mjs
+++ src/js/posts/display-posts.mjs
@@ -1,16 +1,34 @@
 export async function displayPosts(getPosts, container) {
 
     try {
+
         const posts = await getPosts;
         console.log(posts);
+
         for( let i = 0; i < posts.length; i++) {
 
-            console.log(posts.title)
-            const newPost = document.createElement("section");
-            newPost.innerHTML = `<h2>${posts[i].title}</h2>`
+            let image = posts[i].media;
+            if(!image) {
+                image = "/src/assets/images/wild-and-free.jpg"
+            };
+
+            const newPost = document.createElement("div");
+
+            newPost.innerHTML = `<div class="bg-white m-2 p-4 border border-info rounded-5 d-flex flex-wrap flex-lg-nowrap flex-xl-nowrap">
+                                   <div class="p-4">
+                                     <h2>${posts[i].title}</h2>
+                                     <figure class="figure d-flex">
+                                       <figcaption class="figure-caption text-primary m-2">${posts[i].id}</figcaption>
+                                       <img src="/src/assets/images/sheephead.jpg" class="text-bg-light figure-img img-fluid rounded-circle text-bg-primary">
+                                     </figure>
+                                     <p>${posts[i].body}</p>
+                                   </div>
+                                   <div class="p-4">
+                                     <img src="${image}" class="img-fluid rounded-3" alt="Responsive image" style="max-height: 250px">
+                                   </div>
+                                 </div>`
 
             container.appendChild(newPost);
-            //console.log(container)
         };
 
     } catch(error) {
