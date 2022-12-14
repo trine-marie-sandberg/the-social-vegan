87fe0c1721e3d62cc84a3d4159297037b477fdbc
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-12-14 12:28:15 +0100

    problem fix for duplicate btns dete post function

--- src/js/posts/delete-post.mjs
+++ src/js/posts/delete-post.mjs
@@ -4,29 +4,28 @@ export function deletePost(deleteBtn, id, deleteModal) {
 
     const deleteUrl = `https://nf-api.onrender.com/api/v1/social/posts/${id}`;
 
-    deleteBtn.addEventListener("click", (event) => {
-        
-        //{ MODAL COMPONMENTS
-        deleteModal.style.display = "block";
-        console.log(deleteUrl);
-        const token = storageLoad("accessToken");
-
+    //{ MODAL COMPONMENTS
         const dialogText = document.createElement("h2");
         dialogText.innerText = "Delete this post?"
         deleteModal.appendChild(dialogText);
-                                
+                                    
         const noBtn = document.createElement("button");
         const btnClasses = ["btn", "btn-primary", "btn-md", "text-secondary", "m-4"];
         noBtn.classList.add(...btnClasses);
         noBtn.innerText = "No";
         deleteModal.appendChild(noBtn);
-
+    
         const confirmBtn = document.createElement("button");
         confirmBtn.classList.add(...btnClasses);
         confirmBtn.innerText = "Delete";
         confirmBtn.style.backgroundColor = "darkred";
         deleteModal.appendChild(confirmBtn);
-        //}
+    //}
+    deleteModal.style.display = "none";
+
+    deleteBtn.addEventListener("click", () => {
+
+        deleteModal.style.display = "block";
         const delNo = noBtn;
         delNo.addEventListener("click", () => {
             deleteModal.style.display = "none";
@@ -36,6 +35,7 @@ export function deletePost(deleteBtn, id, deleteModal) {
         delComfirm.onclick = async function removePost() {
 
             try {
+                const token = storageLoad("accessToken");
                 const postData = {
                     method: "DELETE",
 
