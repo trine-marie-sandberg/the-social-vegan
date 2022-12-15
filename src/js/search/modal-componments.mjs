1d7f5f198705252f32b43d1b9a8f74fb0613af06
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-12-15 11:10:32 +0100

    convert searchString to lowercase

---
+++ src/js/search/modal-componments.mjs
@@ -0,0 +1,7 @@
+export function searchContainer() {
+    
+    const searchListContainer = document.createElement("div");
+    const containerClasses = ["p-4"];
+    searchListContainer.classList.add(...containerClasses);
+    return searchListContainer;
+};
--- src/js/search/search.mjs
+++ src/js/search/search.mjs
@@ -1,25 +1,24 @@
 import { getPosts } from "/src/js/posts/get-posts.mjs"
 import { storageLoad } from "../storage/localstorage.mjs";
+import { searchContainer } from "./modal-componments.mjs";
 
 export async function searchHandler(searchBar) {
 
-    const allPostUrl = "https://nf-api.onrender.com/api/v1/social/posts/?_author=true";
     let postArray = [];
 
     //GET ALL POSTS
+    const allPostUrl = "https://nf-api.onrender.com/api/v1/social/posts/?_author=true";
     const userToken = storageLoad("accessToken");
-    
     postArray = await getPosts(allPostUrl, userToken);
 
-    const searchListContainer = document.createElement("div");
-    const containerClasses = ["p-4"];
-    searchListContainer.classList.add(...containerClasses);
+    const searchListContainer = searchContainer();
     document.querySelector("header").appendChild(searchListContainer);
     searchListContainer.style.display = "none";
 
     searchBar.addEventListener("keyup", (event) => {
         //FILTER POSTARRAY AFTER SEARCHBAR VALUE
-        const searchString = event.target.value;
+        const searchCharacters = event.target.value;
+        const searchString = searchCharacters.toLowerCase();
         
         let filteredPosts = postArray.filter(post => {
             
@@ -43,20 +42,21 @@ export async function searchHandler(searchBar) {
     const postList = (postArray) => {
         
         const htmlString = postArray.map((post) => {
+
             let title = post.title;
-            title.toLowerCase();
             let image = post.media;
+            
             if(!image) {
                 image = "/src/assets/images/wild-and-free.jpg";
             };
             
-            return `<a href="/post/?id=${post.id}">
-                      <div class="search-results-container">
-                         <li>
+            return `<a href="/post/?id=${post.id}" class="text-decoration-none">
+                      <ul>
+                         <li class="list-style-none">
                            <h2>${title}</h2>
                            <image src="${image}" alt="" class="img-fluid" style="height: 80px;">
                          </li>
-                       </div>
+                       </ul>
                     <a>`;
         }).join("");
     
